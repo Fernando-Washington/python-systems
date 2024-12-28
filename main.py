e23aae16c3be4cb289a26551890385843f6bf2dd
Fernando <ironitabarbosa@gmail.com>
2024-12-27 23:34:43 -0400

    atualizar estrutura de código e funcionalidade

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-from resorces import mensagem
+from resources import mensagem, menu_opcoes
+from alunos import cadastar_aluno
+from professores import cadastrar_professor
+from disciplinas import cadastrar_disciplina
+from turmas import cadastrar_turma
 
 mensagem("Sistema Acadêmico")
 
+opcoes = {
+    1: cadastar_aluno,
+    2: cadastrar_professor,
+    3: cadastrar_disciplina,
+    4: cadastrar_turma
+}
 
+while True:
+    mensagem("Menu")
+    menu_opcoes()
+
+    opcao_escolhida = int(input("Digite a opção desejada: "))
+
+    if opcao_escolhida == 5:
+        print("Saindo do sistema...")
+        break
+    elif opcao_escolhida in opcoes:
+        opcoes[opcao_escolhida]()
+    else:
+        print("Opção inválida. Tente novamente.")
+        
+        # Se opcao_escolhida estiver em opcoes, a condição é verdadeira e o código dentro do if será executado.
 
