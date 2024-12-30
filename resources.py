965a15c26f2a94f8fcc981dcbf4ec026faa52ec6
Fernando <ironitabarbosa@gmail.com>
2024-12-29 23:48:26 -0400

    adicionando novas opcoes de menu ao main.py

--- resources.py
+++ resources.py
@@ -8,13 +8,19 @@ def mensagem(msg):
 
 # exibe opcoes do menu principal
 def menu_opcoes():
-    print("------------------------------")
-    print("-  1 - Cadastrar Aluno       -")
-    print("-  2 - Cadastrar Professor   -")
-    print("-  3 - Cadastrar Disciplina  -")
-    print("-  4 - Cadastrar Turma       -")
-    print("-  5 - Sair                  -")
-    print("------------------------------")
+    print("----------------------------------------------")
+    print("-  1  - Cadastrar Aluno                      -")
+    print("-  2  - Cadastrar Professor                  -")
+    print("-  3  - Cadastrar Disciplina                 -")
+    print("-  4  - Cadastrar Turma                      -")
+    print("-  5  - Matricular Aluno em Turma            -")
+    print("-  6  - Alocar Professor em Disciplina       -")
+    print("-  7  - Alocar Disciplina em Turma           -")
+    print("-  8  - Consultar Professores por Disciplina -")
+    print("-  9  - Consultar Alunos por Turma           -")
+    print("-  10 - Consultar Disciplinas por Turma      -")
+    print("-  11 - Sair                                 -")
+    print("----------------------------------------------")
 
 # função para gerar código no formato A1234
 def gerar_codigo():
