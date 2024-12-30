e21d7f72f247469969c39c1edc9588d846db7f90
Fernando <ironitabarbosa@gmail.com>
2024-12-29 23:51:29 -0400

    criação de arquivos globais afim de resolver conflitos de 'importação circular'

---
+++ dados_globais.py
@@ -0,0 +1,6 @@
+# fiquei algumas horas preso em uma "importação circular de módulos" aonde eu importava matricular_alunos para main.py e main.py para matricular_alunos.
+# acabei descobrindo da pior forma possível, mas consegui resolver. criando uma variável global para armazenar o valor de matricula_aluno e passando-a como argumento para a função matricular_aluno.
+
+lista_alunos = []
+lista_turmas = []
+lista_professores = []
