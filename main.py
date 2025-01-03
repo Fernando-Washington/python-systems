f0075ac80527e6d79f52e9f0bfbdbdd924d7ad24
Fernando <ironitabarbosa@gmail.com>
2025-01-03 19:06:53 -0400

    adicionar novas funcionalidades e implementa novas funções e outrar em testes

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from professores import cadastrar_professor
 from disciplinas import cadastrar_disciplina
 from turmas import cadastrar_turma
 from matricular_alunos import matricular_aluno_em_turma
-#from alocar_professor import alocar_professor_em_disciplina
-# erro circular de importação estou tentando...
+from alocar_professor import alocar_professor_em_disciplina
+from alocar_disciplinas import alocar_disciplina_em_turma
+
+# consultas 
 from consultas.consultar_alunos_por_turma import consultar_alunos_por_turma
 from consultas.consultar_disciplinas_por_turma import consultar_disciplinas_por_turma
+from consultas.consultar_professores_por_disciplina import consultar_professores_por_disciplina
 
 # mensagem de boas vindas
 mensagem("Sistema Acadêmico")
@@ -18,8 +21,12 @@ opcoes = {
     2: cadastrar_professor,
     3: cadastrar_disciplina,
     4: cadastrar_turma,
-    5: matricular_aluno_em_turma
-    # 6: alocar_professor[...]
+    5: matricular_aluno_em_turma,
+    6: alocar_professor_em_disciplina,
+    7: alocar_disciplina_em_turma,
+    8: consultar_alunos_por_turma,
+    9: consultar_disciplinas_por_turma,
+    10: consultar_professores_por_disciplina,
 }
 
 # Loop principal do programa para interagir com o usuário
@@ -40,40 +47,37 @@ def main():
             cadastrar_turma()
         elif opcao_escolhida == 5: 
             matricular_aluno_em_turma()
-            print("###### Em teste ######")
-            print("###### Em teste ######")
-            print("###### Em teste ######")
             # funcionando pronto para implementar
         elif opcao_escolhida == 6:
-            print("Não implementado")
-            #alocar_professor_em_disciplina()
-            print("###### Em teste ######")
-            print("###### Em teste ######")
-            print("###### Em teste ######")
+            alocar_professor_em_disciplina()
         elif opcao_escolhida == 7:
             print("Não implementado")
-            '''alocar_disciplina'''
+            alocar_disciplina_em_turma()
+            print("###### Em teste ######")
+            print("###### Em teste ######")
+            print("###### Em teste ######")
         elif opcao_escolhida == 8:
             print("Não implementado")
+            consultar_professores_por_disciplina()
+            print("###### Em teste ######")
+            print("###### Em teste ######")
+            print("###### Em teste ######")
             '''consultar_professores'''
         elif opcao_escolhida == 9:
             print("###### Em teste ######")
             print("###### Em teste ######")
             print("###### Em teste ######")
-            '''consultar_alunos'''
             consultar_alunos_por_turma()
         elif opcao_escolhida == 10:
             print("###### Em teste ######")
             print("###### Em teste ######")
             print("###### Em teste ######")
-            '''consultar_disciplinas'''
             consultar_disciplinas_por_turma()
         elif opcao_escolhida == 11:
             print("Saindo do programa...")
             break
         else:
-            print("Opção inválida. Tente novamente.")
-            
+            print("Opção inválida. Tente novamente.") 
             # os ifs são apenas para testes depois volto com a seleção por dicionário
 main()
 
