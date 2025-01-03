c94d0083f7f93d88ec169a4ac41a9ee52d435992
Fernando <ironitabarbosa@gmail.com>
2025-01-02 23:00:09 -0400

    novas funcionalidades em teste

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from turmas import cadastrar_turma
 from matricular_alunos import matricular_aluno_em_turma
 #from alocar_professor import alocar_professor_em_disciplina
 # erro circular de importação estou tentando...
+from consultas.consultar_alunos_por_turma import consultar_alunos_por_turma
+from consultas.consultar_disciplinas_por_turma import consultar_disciplinas_por_turma
 
 # mensagem de boas vindas
 mensagem("Sistema Acadêmico")
@@ -33,9 +35,7 @@ def main():
         elif opcao_escolhida == 2:
             cadastrar_professor()
         elif opcao_escolhida == 3:
-            cadastrar_disciplina()
-            print("em teste")
-            # funcionando pronto para habilitar funcao   
+            cadastrar_disciplina() 
         elif opcao_escolhida == 4:
             cadastrar_turma()
         elif opcao_escolhida == 5: 
@@ -43,6 +43,7 @@ def main():
             print("###### Em teste ######")
             print("###### Em teste ######")
             print("###### Em teste ######")
+            # funcionando pronto para implementar
         elif opcao_escolhida == 6:
             print("Não implementado")
             #alocar_professor_em_disciplina()
@@ -56,11 +57,17 @@ def main():
             print("Não implementado")
             '''consultar_professores'''
         elif opcao_escolhida == 9:
-            print("Não implementado")
+            print("###### Em teste ######")
+            print("###### Em teste ######")
+            print("###### Em teste ######")
             '''consultar_alunos'''
+            consultar_alunos_por_turma()
         elif opcao_escolhida == 10:
-            print("Não implementado")
+            print("###### Em teste ######")
+            print("###### Em teste ######")
+            print("###### Em teste ######")
             '''consultar_disciplinas'''
+            consultar_disciplinas_por_turma()
         elif opcao_escolhida == 11:
             print("Saindo do programa...")
             break
