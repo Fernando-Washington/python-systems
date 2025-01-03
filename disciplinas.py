d7b4f10fb6b21fa6875004acc05d3c045f1ea02f
Fernando <ironitabarbosa@gmail.com>
2025-01-02 23:02:37 -0400

    correções visuais

--- disciplinas.py
+++ disciplinas.py
@@ -4,7 +4,7 @@ from professores import professores
 
 # cadastro de disciplinas
 def cadastrar_disciplina():
-    mensagem("Cadastar de Disciplina")
+    mensagem("Cadastar Disciplina")
     
     nome_disciplina = input("Digite o nome da disciplina: \n")
     codigo = gerar_codigo()
@@ -12,9 +12,9 @@ def cadastrar_disciplina():
 
     # logica para vincular professor a disciplina
     if professores: # se a lista nãoe stiver vazia retornará True
-        print("Professores já cadastrados: \n")
+        print("Professores já cadastrados: ")
         for i, item in enumerate(professores): 
-            print(f"{i + 1} {item['nome']}") 
+            print(f"{i + 1} - {item['nome']}") 
             # exibindo os professores cadastrados para garantir que nomes de disciplinas de forma numerada
             escolha = int(input("Selecione o número do professor respomsável pela disciplina \n")) - 1 # -1 pois a lista começa do 0
         if 0 <= escolha < len(professores):    
