394642f2b89c81c218b964c8f995682da05fcb3c
Fernando <ironitabarbosa@gmail.com>
2025-01-02 23:03:41 -0400

    adicionando suporte para correção

--- dados_globais.py
+++ dados_globais.py
@@ -2,6 +2,13 @@
 # acabei descobrindo da pior forma possível, mas consegui resolver. criando uma variável global para armazenar o valor de matricula_aluno e passando-a como argumento para a função matricular_aluno.
 
 lista_alunos = []
-lista_turmas = []
+lista_turmas = [
+    {
+        'nome': '',
+        'codigo': '',
+        'alunos': [],
+        'disciplinas': [],
+    },  
+]
 lista_professores = []
 lista_disciplinas = []
