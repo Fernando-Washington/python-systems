2bf1fc60b2344d769570eb856233442126420e84
Fernando <ironitabarbosa@gmail.com>
2025-01-05 19:02:03 -0400

    adicionar exemplos de entrada

--- dados_globais.py
+++ dados_globais.py
@@ -1,11 +1,13 @@
-# fiquei algumas horas preso em uma "importação circular de módulos" aonde eu importava matricular_alunos para main.py e main.py para matricular_alunos.
-# acabei descobrindo da pior forma possível, mas consegui resolver. criando uma variável global para armazenar o valor de matricula_aluno e passando-a como argumento para a função matricular_aluno.
+'''
+fiquei algumas horas preso em uma "importação circular de módulos" aonde eu importava matricular_alunos para main.py e main.py para matricular_alunos.
+acabei descobrindo da pior forma possível, mas consegui resolver. criando uma variável global para armazenar o valor de matricula_aluno e passando-a como argumento para a função matricular_aluno.
+'''
 
 lista_alunos = []
 lista_turmas = [
     {
-        'nome': '',
-        'codigo': '',
+        'nome': 'TADS 01',
+        'codigo': 'T2501',
         'alunos': [],
         'disciplinas': [],
     },  
