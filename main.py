64a5e906be2750b28f513ca4da0415cd3a545b7c
Fernando <ironitabarbosa@gmail.com>
2025-01-05 19:02:53 -0400

    correção de seleção + tratamento de exceção

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ opcoes = {
     5: matricular_aluno_em_turma,
     6: alocar_professor_em_disciplina,
     7: alocar_disciplina_em_turma,
-    8: consultar_alunos_por_turma,
-    9: consultar_disciplinas_por_turma,
-    10: consultar_professores_por_disciplina,
+    8: consultar_professores_por_disciplina,
+    9: consultar_alunos_por_turma,
+    10: consultar_disciplinas_por_turma,
 }
 
 # Loop principal do programa para interagir com o usuário
@@ -34,50 +34,18 @@ def main():
     while True:
         mensagem("Menu Principal")
         menu_opcoes()
+        try:
+            opcao_escolhida = int(input("Digite a opção desejada: "))
 
-        opcao_escolhida = int(input("Digite a opção desejada: "))
-
-        if opcao_escolhida == 1: 
-            cadastar_aluno()
-        elif opcao_escolhida == 2:
-            cadastrar_professor()
-        elif opcao_escolhida == 3:
-            cadastrar_disciplina() 
-        elif opcao_escolhida == 4:
-            cadastrar_turma()
-        elif opcao_escolhida == 5: 
-            matricular_aluno_em_turma()
-            # funcionando pronto para implementar
-        elif opcao_escolhida == 6:
-            alocar_professor_em_disciplina()
-        elif opcao_escolhida == 7:
-            print("Não implementado")
-            alocar_disciplina_em_turma()
-            print("###### Em teste ######")
-            print("###### Em teste ######")
-            print("###### Em teste ######")
-        elif opcao_escolhida == 8:
-            print("Não implementado")
-            consultar_professores_por_disciplina()
-            print("###### Em teste ######")
-            print("###### Em teste ######")
-            print("###### Em teste ######")
-            '''consultar_professores'''
-        elif opcao_escolhida == 9:
-            print("###### Em teste ######")
-            print("###### Em teste ######")
-            print("###### Em teste ######")
-            consultar_alunos_por_turma()
-        elif opcao_escolhida == 10:
-            print("###### Em teste ######")
-            print("###### Em teste ######")
-            print("###### Em teste ######")
-            consultar_disciplinas_por_turma()
-        elif opcao_escolhida == 11:
-            print("Saindo do programa...")
-            break
-        else:
-            print("Opção inválida. Tente novamente.") 
-            # os ifs são apenas para testes depois volto com a seleção por dicionário
+            if opcao_escolhida in opcoes:
+                opcoes[opcao_escolhida]()
+            elif opcao_escolhida == 11:
+                print("Saindo do programa...")
+                break
+            else:
+                print("Opção inválida. Tente novamente.")
+                
+        except ValueError:
+                print("Opção inválida. Tente novamente.") 
 main()
 
